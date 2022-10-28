7ea85ce77dbcfe57f3b17e1eafe6f33f3695c487
Joar Gruneau <joar@gruneau.se>
2022-10-28 16:55:54 +0200

    added installer script

---
+++ installer.sh
@@ -0,0 +1,5 @@
+sudo add-apt-repository ppa:kelleyk/emacs
+apt-get update
+apt-get install -y emacs28
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+git clone git@github.com:JoarGruneau/spacemacs-configs.git .spacemacs.d
